02a2396888c9dd4b1cfceb494c4b412f5337068d
wayneasilva <wayneasilva@gmail.com>
2018-11-11 18:13:01 -0500

    changed port

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ app.get("/", function(req, res) {
     res.render("index");
 })
 
-// app.listen(process.env.PORT, process.env.IP);
-app.listen(3000);
+app.listen(process.env.PORT, process.env.IP);
+// app.listen(3000);
