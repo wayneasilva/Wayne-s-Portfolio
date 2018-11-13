95dfcea0667cce86260253690fa0bd9113366c2c
wayneasilva <wayneasilva@gmail.com>
2018-11-12 23:29:09 -0500

    complete basic clock and date/time component

--- public/works/HOKAI/scripts/scripts.js
+++ public/works/HOKAI/scripts/scripts.js
@@ -1,16 +1,71 @@
-function clockDisplay() {
+function clock() {
     const date = new Date();
+
+    const dayOfWeek = date.getDay();
+    const month = date.getMonth();
+    const dayOfMonth = date.getDate();
+    const year = date.getFullYear();
+
     const hours =  date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
 
-    const hourDisplay = document.querySelector("#hour");
-    const minuteDisplay = document.querySelector("#minute");
-    const secondDisplay = document.querySelector("#second")
+    const hourDisplay = document.querySelector("#hourDisplay");
+    const minuteDisplay = document.querySelector("#minuteDisplay");
+    const secondDisplay = document.querySelector("#secondDisplay");
 
-    hourDisplay.textContent = hours;
-    minuteDisplay.textContent = minutes;
-    secondDisplay.textContent = seconds
+    const dayOfWeekDisplay = document.querySelector("#dayOfWeekDisplay")
+    const monthDisplay = document.querySelector("#monthDisplay");
+    const dayDisplay = document.querySelector("#dayofMonthDisplay");
+    const yearDisplay = document.querySelector("#yearDisplay");
+
+    const weekDays = 
+                    {
+                        "0": "Mon",
+                        "1": "Tues",
+                        "2": "Wed",
+                        "3": "Thur",
+                        "4": "Fri",
+                        "5": "Sat",
+                        "6": "Sun"
+                    };
+    const months = 
+                    {
+                        "0": "January",
+                        "1": "February",
+                        "2": "March",
+                        "3": "April",
+                        "4": "May",
+                        "5": "June",
+                        "6": "July",
+                        "7": "August",
+                        "8": "September",
+                        "9": "October",
+                        "10": "November",
+                        "11": "December"
+                    }
+
+    function setDateTime() {
+        let monthNum = month;
+        let day = dayOfWeek;
+
+        if (months.hasOwnProperty(monthNum)) {
+            monthDisplay.textContent = months[monthNum];
+        }
+
+        if (weekDays.hasOwnProperty(day)) {
+            dayOfWeekDisplay.textContent = weekDays[day];
+        }
+
+        hourDisplay.textContent = hours;
+        minuteDisplay.textContent = minutes;
+        secondDisplay.textContent = seconds;
+
+        dayOfMonthDisplay.textContent = dayOfMonth;
+        yearDisplay.textContent = year;
+    }
+
+    setDateTime();
 }
 
-setInterval(clockDisplay, 100);
+setInterval(clock, 1000);
